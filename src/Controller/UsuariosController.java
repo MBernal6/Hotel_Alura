eb9ea5cbcedc5309548ec4bcdcf8fd2f9eeb120b
Mauricio <mbernalp@unsa.edu.pe>
2023-04-25 10:56:15 -0500

    Modificacion Login y creacion de UsuarioController

---
+++ src/Controller/UsuariosController.java
@@ -0,0 +1,35 @@
+package Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+import Modelo.Usuarios;
+import views.Login;
+import views.MenuUsuario;
+
+public class UsuariosController  implements ActionListener{
+
+	private Login vista;
+	
+	public UsuariosController(Login vista) {
+		this.vista = vista; 
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String nombre = vista.getNombre(); 
+		String contraseña = vista.getContraseña();
+		
+		if(Usuarios.validarUsuario(nombre, contraseña)) {
+			MenuUsuario menu = new MenuUsuario();
+			menu.setVisible(true);
+			vista.dispose();
+		}else {
+			JOptionPane.showMessageDialog(vista, "Usuario o contraseña no validos");
+		}
+			
+	}
+	
+}
--- src/Modelo/Usuarios.java
+++ src/Modelo/Usuarios.java
@@ -1,5 +1,12 @@
 package Modelo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Factory.ConexionBase;
+
 public class Usuarios {
 
 	private String nombre;
@@ -24,5 +31,34 @@ public class Usuarios {
 
 	public void setContraseña(String contraseña) {
 		this.contraseña = contraseña;
+	}
+	public static boolean validarUsuario(String nombre, String contraseña) {
+		ConexionBase con = new ConexionBase();
+		Connection connec = null;
+		PreparedStatement state = null;
+		ResultSet result = null; 
+		try {
+			connec = con.conectarBase();
+			state = connec.prepareStatement(" SELECT * FROM usuarios WHERE nombre =? AND contraseña =?");
+			state.setString(1, nombre);
+			state.setString(2, contraseña);
+			result = state.executeQuery();
+			return result.next();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			try {
+				if(result != null)
+					result.close();
+				if(state != null)
+					state.close();
+				if(connec != null)
+					connec.close();
+			}catch (SQLException e2) {
+				e2.printStackTrace();
+			}
+		}
+		
 	}
 }
